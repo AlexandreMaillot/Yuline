b4f7c6fa91a73cf9ab21592762a1ca8453f5ded7
Jérémy LEGROS <jlegros.cdi@FORMATION.LOCAL>
2019-02-07 11:20:25 +0400

    Ajout des components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnectionComponent } from './connection/connection.component';
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DeckComponent } from './deck/deck.component';
+import { FiltersComponent } from './filters/filters.component';
+import { DeckListComponent } from './deck-list/deck-list.component';
+import { CardComponent } from './card/card.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { CardInfoComponent } from './card-info/card-info.component';
+import { SideDeckComponent } from './side-deck/side-deck.component';
+import { ExtraDeckComponent } from './extra-deck/extra-deck.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectionComponent,
+    HomeComponent,
+    RegistrationComponent,
+    ProfileComponent,
+    DeckComponent,
+    FiltersComponent,
+    DeckListComponent,
+    CardComponent,
+    CardListComponent,
+    CardInfoComponent,
+    SideDeckComponent,
+    ExtraDeckComponent,
   ],
   imports: [
     BrowserModule,
