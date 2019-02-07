bc7812d03156f56d72e2b67e9ad761094a76ac2e
tamaroc <tamoro974@gmail.com>
2019-02-07 10:12:27 +0400

    test 0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,6 @@ const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]atfhyb
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
