ae2af7e45ab4cf10c324830d0c2d0a9a8b269dc3
AlexandreMaillot <alexandre.maillot97@gmail.com>
2019-02-08 16:30:21 +0400

    Récuperation des cartes

--- src/app/Components/card-list/card-list.component.ts
+++ src/app/Components/card-list/card-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CardService} from "../../Service/card.service";
 
 @Component({
   selector: 'app-card-list',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private CarteService: CardService) { }
 
   ngOnInit() {
+    this.CarteService.getAllCards().then( cartes => console.log(cartes))
   }
 
 }
--- src/app/Components/deck-manage/deck-manage.component.ts
+++ src/app/Components/deck-manage/deck-manage.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {CardListComponent} from "../card-list/card-list.component";
 export interface Tile {
   cols: number;
   rows: number;
   text: string;
+  componentAdd: string;
 }
 @Component({
   selector: 'app-deck-manage',
@@ -11,16 +13,14 @@ export interface Tile {
 })
 export class DeckManageComponent implements OnInit {
   tiles: Tile[] = [
-    {text: 'One', cols: 6, rows: 2},
-    {text: 'One', cols: 5, rows: 2},
-    {text: 'One', cols: 5, rows: 6},
-    {text: 'Two', cols: 4, rows: 9},
-    {text: 'Three', cols: 7, rows: 5},
-    {text: 'Four', cols: 5, rows: 5},
-    {text: 'Four', cols: 7, rows: 2},
-    {text: 'Four', cols: 7, rows: 2},
-
-
+    {text: 'Liste Deck', cols: 6, rows: 2, componentAdd: "<app-filters></app-filters>"},
+    {text: 'Filter', cols: 5, rows: 2, componentAdd: "<app-filters></app-filters>"},
+    {text: 'Carte', cols: 5, rows: 6, componentAdd: "<app-filters></app-filters>"},
+    {text: 'Resultat', cols: 4, rows: 9, componentAdd: "<app-filters></app-filters>"},
+    {text: 'Deck', cols: 7, rows: 5, componentAdd: "<app-filters></app-filters>"},
+    {text: 'Description', cols: 5, rows: 5, componentAdd: "<app-filters></app-filters>"},
+    {text: 'Side-Deck', cols: 7, rows: 2, componentAdd: "<app-filters></app-filters>"},
+    {text: 'Extra-Deck', cols: 7, rows: 2, componentAdd: "<app-filters></app-filters>"}
 
   ];
   constructor() { }
