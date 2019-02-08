58e9d484969911fcb5058882f3a202e8b31e6a26
AlexandreMaillot <alexandre.maillot97@gmail.com>
2019-02-08 14:06:20 +0400

    Cadrage deck manage terminé

--- src/app/Components/deck-manage/deck-manage.component.ts
+++ src/app/Components/deck-manage/deck-manage.component.ts
@@ -11,14 +11,14 @@ export interface Tile {
 })
 export class DeckManageComponent implements OnInit {
   tiles: Tile[] = [
-    {text: 'One', cols: 6, rows: 1},
-    {text: 'One', cols: 5, rows: 1},
+    {text: 'One', cols: 6, rows: 2},
     {text: 'One', cols: 5, rows: 2},
-    {text: 'Two', cols: 4, rows: 8},
-    {text: 'Three', cols: 7, rows: 6},
-    {text: 'Four', cols: 5, rows: 7},
-    {text: 'Four', cols: 7, rows: 1},
-    {text: 'Four', cols: 7, rows: 1},
+    {text: 'One', cols: 5, rows: 6},
+    {text: 'Two', cols: 4, rows: 9},
+    {text: 'Three', cols: 7, rows: 5},
+    {text: 'Four', cols: 5, rows: 5},
+    {text: 'Four', cols: 7, rows: 2},
+    {text: 'Four', cols: 7, rows: 2},
 
 
 
