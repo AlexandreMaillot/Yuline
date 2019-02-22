225f9c02693fdcf8d72419637c9e9d81ac307237
Jérémy LEGROS <jlegros.cdi@FORMATION.LOCAL>
2019-02-22 08:54:36 +0400

    lul

--- src/app/Components/card-list/card-list.component.ts
+++ src/app/Components/card-list/card-list.component.ts
@@ -44,7 +44,6 @@ export class CardListComponent implements OnInit {
       // this.deckService.addCardDeck(idCard);
   }
 
-<<<<<<< HEAD
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
@@ -57,6 +56,4 @@ export class CardListComponent implements OnInit {
     console.log('test');
   }
 
-=======
->>>>>>> b466d7b06534ab2ef72f3dbeee7106ea27f1c8cb
 }
